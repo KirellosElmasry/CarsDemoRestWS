61bceeb6520a6d89bf4f3cfe14e0b86d90cd3b91
Kirellos Elmasry <kelmasry@advancedoperations.com>
2018-10-28 15:30:40 +0200

    Add exception handling.

--- src/main/java/com/evolvice/controller/CarController.java
+++ src/main/java/com/evolvice/controller/CarController.java
@@ -9,7 +9,10 @@ import com.evolvice.dao.CarDAO;
 import com.evolvice.entity.Car;
 import java.util.List;
 import java.util.NoSuchElementException;
+import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,18 +38,22 @@ public class CarController {
     }
 
     @GetMapping("/getAllData")
-    public List<Car> getAllCars(@RequestHeader("apikey") String api_key) {
+    public ResponseEntity<List<Car>> getAllCars(@RequestHeader("apikey") String api_key) {
         if (api_key.equals("1234")) {
-
-            return carDAO.findAll();
+            try {
+                return new ResponseEntity<>(carDAO.findAll(), HttpStatus.OK);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+            }
 
         } else {
-            return null;
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
     }
 
     @GetMapping("/insertData")
-    public String insertCarsData(@RequestHeader("apikey") String api_key) {
+    public ResponseEntity<String> insertCarsData(@RequestHeader("apikey") String api_key) {
         if (api_key.equals("1234")) {
             try {
                 for (int i = 1; i < 10; i++) {
@@ -54,73 +61,86 @@ public class CarController {
                 }
             } catch (Exception e) {
                 e.printStackTrace();
-                return "Error when inserting Cars data";
+                return new ResponseEntity<>("Error when inserting Cars data", HttpStatus.EXPECTATION_FAILED);
             }
-            return "Inserted successfully";
+            return new ResponseEntity<>("Inserted successfully", HttpStatus.OK);
         } else {
-            return "Wrong apiKey";
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
     }
 
     @GetMapping("/get/{id}")
-    public Car getCarById(@RequestHeader("apikey") String api_key, @PathVariable("id") long id) {
+    public ResponseEntity<Car> getCarById(@RequestHeader("apikey") String api_key, @PathVariable("id") long id) {
         if (api_key.equals("1234")) {
             try {
-                return carDAO.findById(id).get();
+                return new ResponseEntity<>(carDAO.findById(id).get(), HttpStatus.OK);
             } catch (NoSuchElementException e) {
                 e.printStackTrace();
-                return null;
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
         } else {
-            return null;
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
     }
 
     @GetMapping("/delete/{id}")
-    public String deleteCarById(@RequestHeader("apikey") String api_key, @PathVariable("id") long id) {
+    public ResponseEntity<String> deleteCarById(@RequestHeader("apikey") String api_key, @PathVariable("id") long id) {
         if (api_key.equals("1234")) {
             try {
                 carDAO.deleteById(id);
+            } catch (NoSuchElementException e) {
+                e.printStackTrace();
+                return new ResponseEntity<>(" Car with Id : " + id + " not found in database!", HttpStatus.NOT_FOUND);
             } catch (Exception e) {
                 e.printStackTrace();
-                return "Error when deleting Car with Id : " + id;
+                return new ResponseEntity<>("Error when deleting Car with Id : " + id, HttpStatus.EXPECTATION_FAILED);
             }
-            return "Car with Id : " + id + " deleted successfully!";
+            return new ResponseEntity<>("Car with Id : " + id + " deleted successfully!", HttpStatus.OK);
         } else {
-            return "Wrong apiKey";
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
     }
 
     @RequestMapping(value = {"/update"}, method = RequestMethod.POST, consumes = "application/json")
     @ResponseBody
-    public String updateCar(@RequestHeader("apikey") String api_key, @RequestBody Car car) {
+    public ResponseEntity<String> updateCar(@RequestHeader("apikey") String api_key, @RequestBody Car car) {
         if (api_key.equals("1234")) {
             try {
-                carDAO.saveAndFlush(car);
-            } catch (Exception e) {
-                e.printStackTrace();
-                return "Error when inserting Car";
+                Car newCar = carDAO.findById(car.getCarId()).get();
+
+                if (newCar != null) {
+                    carDAO.saveAndFlush(car);
+                }
             }
-            return "Updated successfully";
+            catch (NoSuchElementException e) {
+                e.printStackTrace();
+                 return new ResponseEntity<>(" Car with Id : " + car.getCarId() + " not found in database!", HttpStatus.NOT_FOUND);             
+            } 
+            catch (Exception e) {
+                e.printStackTrace();
+                return new ResponseEntity<>("Error when inserting Car", HttpStatus.EXPECTATION_FAILED);
+            }
+            return new ResponseEntity<>("Updated successfully", HttpStatus.OK);
+
         } else {
-            return "Wrong apiKey";
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
     }
 
     @RequestMapping(value = {"/add"}, method = RequestMethod.POST, consumes = "application/json")
     @ResponseBody
-    public String addCar(@RequestHeader("apikey") String api_key, @RequestBody Car car) {
+    public ResponseEntity<String> addCar(@RequestHeader("apikey") String api_key, @RequestBody Car car) {
         if (api_key.equals("1234")) {
 
             try {
                 carDAO.saveAndFlush(car);
             } catch (Exception e) {
                 e.printStackTrace();
-                return "error when adding new Car";
+                return new ResponseEntity<>("error when adding new Car", HttpStatus.EXPECTATION_FAILED);
             }
-            return "Car inserted successfully";
+            return new ResponseEntity<>("Car inserted successfully", HttpStatus.OK);
         } else {
-            return "Wrong apiKey";
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
     }
 }
